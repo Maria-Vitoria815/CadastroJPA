0a899b9d5f420851ea40e490b27edc86ca1392e3
Maria Vitória S. de Melo <111195864+Maria-Vitoria815@users.noreply.github.com>
2025-01-27 10:33:30 -0300

    ADD PRODUTO

---
+++ ProjetoMaven/src/main/java/br/edu/ifba/basicas/Produto.java
@@ -0,0 +1,63 @@
+package br.edu.ifba.basicas;
+
+import jakarta.persistence.*;
+import java.io.Serializable;
+import java.util.List;
+
+@Entity
+public class Produto implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String descricao;
+    private int estoque;
+    private double valor;
+
+    @ManyToMany(mappedBy = "produtos")
+    private List<Venda> vendas;
+
+    // Getters e Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public int getEstoque() {
+        return estoque;
+    }
+
+    public void setEstoque(int estoque) {
+        this.estoque = estoque;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public List<Venda> getVendas() {
+        return vendas;
+    }
+
+    public void setVendas(List<Venda> vendas) {
+        this.vendas = vendas;
+    }
+}
